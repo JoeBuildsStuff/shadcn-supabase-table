1e6d7e9ed2d4b3bd1535f3c2f7fe4a5e03082816
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2025-06-21 13:29:20 -0700

    Update registry.json to include new page for DataTableExamplePayments and remove deprecated payments page; enhance data table structure with additional components.

---
+++ registry/data-table/pages/data-table-example-payments/page.tsx
@@ -0,0 +1,9 @@
+import DataTableExamplePayments from "@/components/data-table-example-payments/data-table-example-payments";
+
+export default function Payments() {
+  return (
+    <div className="px-8 py-10">
+      <DataTableExamplePayments />
+    </div>
+  )
+}
