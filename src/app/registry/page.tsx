d57a8cebe6614efb3ef6a904e1ed498589f925e4
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2025-06-21 14:42:20 -0700

    Enhance Payments component layout by refining padding and styles in related files; ensure consistent UI across DataTableExamplePayments and main page.

---
+++ src/app/registry/page.tsx
@@ -0,0 +1,9 @@
+import DataTableExamplePayments from "../../../registry/data-table/components/data-table-example-payments/data-table-example-payments";
+
+export default function Registry() {
+  return (
+    <div className="px-3 py-10">
+      <DataTableExamplePayments />
+    </div>
+  )
+}
