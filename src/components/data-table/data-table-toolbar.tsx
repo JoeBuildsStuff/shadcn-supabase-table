c060368de715cf0e5ee383012a37b75f61260e45
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2025-06-22 17:32:33 -0700

    Enhance DataTableToolbar component by adding clear filters and sorts functionality. Implement a button to reset active filters and sorting, improving user experience and data management. Update imports for new UI components.

--- src/components/data-table/data-table-toolbar.tsx
+++ src/components/data-table/data-table-toolbar.tsx
@@ -1,8 +1,9 @@
-
 import { DataTableViewOptions } from "./data-table-view-options"
 import { Table } from "@tanstack/react-table"
 import DataTableFilter from "./data-table-filter"
 import DataTableSort from "./data-table-sort"
+import { X } from "lucide-react"
+import { Button } from "@/components/ui/button"
 
 interface DataTableToolbarProps<TData> {
     table: Table<TData>
@@ -10,11 +11,26 @@ interface DataTableToolbarProps<TData> {
 
 export default function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>) {
     
+  // Check if there are any active sorts or filters
+  const hasActiveSorts = table.getState().sorting.length > 0
+  const hasActiveFilters = table.getState().columnFilters.length > 0
+  const hasActiveFiltersOrSorts = hasActiveSorts || hasActiveFilters
+
+  const clearAllFiltersAndSorts = () => {
+    table.setSorting([])
+    table.setColumnFilters([])
+  }
+
   return (
     <div className="flex items-center">
       <div className="flex items-center gap-2">
         <DataTableSort table={table} />
         <DataTableFilter table={table} />
+        {hasActiveFiltersOrSorts && (
+          <Button variant="outline" size="icon" onClick={clearAllFiltersAndSorts}>
+            <X className="size-4 cursor-pointer" />
+          </Button>
+        )}
       </div>
 
       <DataTableViewOptions table={table} />
